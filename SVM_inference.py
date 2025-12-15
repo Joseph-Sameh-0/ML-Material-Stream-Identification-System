69121c389bfb5c5003e4c1ac2bf307cf7e437c4d
Salma Mohamed <salmasarhan676@gmail.com>
2025-12-16 01:11:52 +0200

    prediction function done

--- SVM_inference.py
+++ SVM_inference.py
@@ -1,55 +1,34 @@
-import cv2
 import numpy as np
 import joblib
-from sklearn.preprocessing import normalize
-from FeatureLead import extract_features_from_image
+from FeatureLead import CNNFeatureExtractor
 from SVM_Classifier import predict_with_rejection
-from sklearn.decomposition import PCA
+import os
 
 # load the trained SVM model
 svm_model = joblib.load("models/svm_model.pkl")
 scaler = joblib.load("models/scaler.pkl")
-pca = joblib.load("models/hog_pca.pkl")
 
 classes = ["cardboard", "glass", "metal", "paper", "plastic", "trash", "unknown"]
 
+feature_extractor = CNNFeatureExtractor()
 
-def extract_features(image_path):
-    extracted = extract_features_from_image(image_path)
+def classify_image(image_path):
+    extracted = feature_extractor.extract_features(image_path)
     if extracted is None:
         return "unknown", 0.0
 
-    color_features, color_moments, hog_features, lbp_features, texture_features, shape_features, material_features = extracted
+    extracted = extracted.reshape(1, -1)
 
-    # 1) PCA on HOG (match training)
-    hog_pca = pca.transform(hog_features.reshape(1, -1))
+    scaled_features = scaler.transform(extracted)
 
-    # 2) L2-normalize each block (same as training)
-    hog_norm = normalize(hog_pca, norm='l2')
-    color_norm = normalize(color_features.reshape(1, -1), norm='l2')
-    texture_norm = normalize(texture_features.reshape(1, -1), norm='l2')
-    shape_norm = normalize(shape_features.reshape(1, -1), norm='l2')
-    color_moments_norm = normalize(color_moments.reshape(1, -1), norm='l2')
-    material_norm = normalize(material_features.reshape(1, -1), norm='l2')
-    lbp_norm = normalize(lbp_features.reshape(1, -1), norm='l2')
-
-    # 3) Concatenate in the same order as X
-    features = np.hstack((
-        hog_norm, color_norm, texture_norm,
-        shape_norm, color_moments_norm,
-        material_norm, lbp_norm
-    ))
-
-    # 4) Apply the saved StandardScaler
-    features_scaled = scaler.transform(features)
-
+    
     # 5) Predict with rejection
-    class_id, confidence = predict_with_rejection(svm_model, features_scaled, threshold=0.55)
+    class_id, confidence = predict_with_rejection(svm_model, scaled_features, threshold=0.55)
     predicted_label = "unknown" if class_id == 6 else classes[class_id]
     return predicted_label, confidence
 
 
 if __name__ == "__main__":  
-    test_image_path = "test_images/woman.jpeg"  
-    predicted_class, conf = extract_features(test_image_path)
+    test_image_path = "test_images/glass2.png"  
+    predicted_class, conf = classify_image(test_image_path)
     print(f"Predicted Class: {predicted_class}, Confidence: {conf:.4f}")
