40ebe703ac6e30f487ff69ddb544055bae1e5f62
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-12-18 16:09:12 +0200

    Refactor SVM inference initialization and remove .gitkeep files

--- SVM_inference.py
+++ SVM_inference.py
@@ -7,11 +7,11 @@ from FeatureLead import CNNFeatureExtractor
 classes = ["cardboard", "glass", "metal", "paper", "plastic", "trash", "unknown"]
 
 
-def classify_image(image_path):
-    svm_model = joblib.load("models/svm_model.pkl")
-    scaler = joblib.load("models/scaler.pkl")
+feature_extractor = CNNFeatureExtractor()
+svm_model = joblib.load("models/svm_model.pkl")
+scaler = joblib.load("models/scaler.pkl")
 
-    feature_extractor = CNNFeatureExtractor()
+def classify_image(image_path):
 
     extracted = feature_extractor.extract_features(image_path)
     if extracted is None:
@@ -21,7 +21,6 @@ def classify_image(image_path):
 
     scaled_features = scaler.transform(extracted)
 
-    
     class_id, confidence = predict_with_rejection(svm_model, scaled_features, threshold=0.55)
     predicted_label = "unknown" if class_id == 6 else classes[class_id]
     return predicted_label, confidence
